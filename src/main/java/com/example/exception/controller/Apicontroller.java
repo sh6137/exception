30c0daa540eef256069eb398a891b676de816693
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-05-25 22:01:48 +0900

    feat : boot validation

--- src/main/java/com/example/exception/controller/Apicontroller.java
+++ src/main/java/com/example/exception/controller/Apicontroller.java
@@ -2,21 +2,33 @@ package com.example.exception.controller;
 
 import com.example.exception.dto.User;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class Apicontroller {
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age) {
+
         User user = new User();
         user.setName(name);
         user.setAge(age);
 
-        int a = 10+age;
+        int a = 10 + age;
 
         return user;
     }
@@ -24,11 +36,5 @@ public class Apicontroller {
     @PostMapping("")
     public User post(@Valid @RequestBody User user) {
         return user;
-    }
-
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
-        System.out.println("controller");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
---
+++ src/main/java/com/example/exception/dto/Error.java
@@ -0,0 +1,13 @@
+package com.example.exception.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Error {
+    private String field;
+    private String message;
+    private String invalidValue;
+
+}
